16469484414b0505d5f5cd03e8cf567f7d9cd2c2
mike929 <mike929@gmail.com>
2019-02-22 22:25:36 -0500

    friends

---
+++ app/data/friends.js
@@ -0,0 +1,75 @@
+//potential friend, photos and scores
+var friends = 
+[
+  {
+    "name":"Taz",
+    "photo":"http://darlenefranklinwrites.com/wp-content/uploads/2018/01/luxury-tasmanian-devil-looney-tunes-wallpaper-taz-the-tasmanian-devil-wallpaper-wallpaper-13-tasmanian-devil-looney-tunes-wallpaper.jpg",
+    "scores":[4, 5, 3, 2, 1, 4, 5, 3, 2, 1]
+  },
+
+  {
+    "name":"Marvin",
+    "photo":"https://upload.wikimedia.org/wikipedia/en/thumb/d/d5/Marvin_the_Martian.svg/1200px-Marvin_the_Martian.svg.png0",
+    "scores":[4, 5, 5, 2, 3, 1, 3, 3, 2, 4] 
+
+  },
+
+  {
+    "name":"Bugs",
+    "photo":"https://www.wonderslist.com/wp-content/uploads/2015/01/Bugs-Bunny-Looney-Tunes-Characters.jpg",
+    "scores":[4, 2, 1, 5, 3, 3, 1, 5, 3, 2] 
+
+  },
+
+  {
+    "name":"Daffy",
+    "photo":"https://upload.wikimedia.org/wikipedia/en/thumb/f/f4/Daffy_Duck.svg/1200px-Daffy_Duck.svg.png",
+    "scores":[4, 2, 5, 2, 3, 5, 5, 3, 1, 3] 
+
+  },
+
+  {
+    "name":"Foghorn",
+    "photo":"https://previews.123rf.com/images/chutimakuanamon/chutimakuanamon1805/chutimakuanamon180500320/104748132-foghorn-leghorn-looney-tunes-character-cartoon-illustration-dacing.jpg",
+    "scores":[2, 3, 2, 5, 5, 5, 5, 4, 3, 1] 
+
+  },
+
+  {
+    "name":"Wiley",
+    "photo":"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjT5O2R89DgAhUin-AKHQdJCvsQjRx6BAgBEAU&url=https%3A%2F%2Fwww.pinterest.com%2Ftugboat15%2Flooney-tunes-characters%2F&psig=AOvVaw20uqGdkoaLWvaVhacdVKfh&ust=1550978389314621",
+    "scores":[2, 3, 2, 5, 5, 5, 5, 4, 3, 1] 
+
+  },
+
+  {
+    "name":"Tweety",
+    "photo":"https://rujabes.files.wordpress.com/2012/11/tweety.png",
+    "scores":[5, 4, 3, 3, 4, 5, 1, 4, 2, 3]
+
+  },
+
+  {
+    "name":"Road Runner",
+    "photo":"https://upload.wikimedia.org/wikipedia/en/thumb/e/ee/Roadrunner_looney_tunes.png/220px-Roadrunner_looney_tunes.png",
+    "scores":[3, 4, 2, 3, 2, 4, 1, 2, 3, 3]
+
+  },
+
+  {
+    "name":"Granny",
+    "photo":"https://upload.wikimedia.org/wikipedia/en/thumb/c/ca/Granny_%28Looney_Tunes%29.png/165px-Granny_%28Looney_Tunes%29.png",
+    "scores":[1, 2, 4, 3, 3, 5, 2, 3, 3, 4]
+
+  },
+
+  {
+    "name":"Henery",
+    "photo":"https://d1o51r9qdgnnlz.cloudfront.net/static_image/beef38cd-21cd-47fc-bfa9-ec67a401329f/static.jpg",
+    "scores":[1, 1, 5, 4, 2, 4, 4, 3, 2, 4]
+
+  }
+
+];
+
+module.exports = friends;
